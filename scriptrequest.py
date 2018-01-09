fc6cb8f8e6c99c7b80857c3bf47a9eea1c3cf05a
Grace Newman <grace.newman@digital.hmrc.gov.uk>
2018-01-09 10:41:03 +0000

    Added comments

--- scriptrequest.py
+++ scriptrequest.py
@@ -4,14 +4,19 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+# Creates a graph, called in the get_weather function at the end
 def make_graph(weather, city):
     temprange = plt.axes()
+    # Alpha gives transparency, lets you see if anything is being covered up
     temprange.bar([1.5, 2.5, 3.5, 4.5, 5.5], weather['Min'], width=0.5, alpha=0.4)
+    # Deals with the issue of missing data, using an if statement
     if len(weather['Max']) == 4:
         temprange.bar([1, 2, 3, 4], weather['Max'], width=0.5, alpha=0.4)
     elif len(weather['Max']) == 5:
         temprange.bar([1, 2, 3, 4, 5], weather['Max'], width=0.5, alpha=0.4)
+    # Labels the x axis with the days
     temprange.set_xticklabels(weather['Day'])
+    # Sets the limits of the y axis, from the minimum or 0 (whatever is lower) to the maximum +1 (so that the maximum can be seen)
     temprange.set_ylim([min(weather['Min'].min(),0), weather['Max'].max()+1])
     temprange.set_ylabel('Temp in degrees')
     temprange.set_title('Weekly Temperature Fluctuations in ' + city[0])
@@ -33,8 +38,9 @@ def get_weather(url):
 
     # If length of max list is 4 (i.e. it's nighttime) do this:
     if len(maxs) == 4:
-        weather=-999*np.ones((5,3), dtype='object')
+        weather=-999*np.ones((5,3), dtype='object') # Deals with the missing data issue, sets any missing data as -999 so it can't be mixed up
         weather[:,0] = day
+        # Note that this is different from the elif statement below: [1:,1] instead of [:,1]. Handling the missing data issue
         weather[1:,1] = [int(i) for i in maxs]
         weather[:,2] = [int(i) for i in mins]
 
@@ -42,17 +48,18 @@ def get_weather(url):
     elif len(maxs) == 5:
         weather=np.zeros((5,3), dtype='object')
         weather[:,0] = day
-        weather[:,1] = [int(i) for i in maxs]
+        weather[:,1] = [int(i) for i in maxs] # Converts data to an integer, similar to the below 0 function. Error runs without this line
         weather[:,2] = [int(i) for i in mins]
 
+    # Masks any -999 in weather, fill value means to blank them out
     weather=np.ma.masked_array(weather, mask=(weather==-999), fill_value=0)
     # Print title
-    print (city[0] + " five day forecast")
+    print (city[0] + " five day forecast") # City is an array with one item, to get the city name as we want it we need city[0]
     # Print the data stuff
     print (weather)
     # Make a file
-    weather = pd.DataFrame(weather, columns=['Day', 'Max', 'Min'])
-    weather.to_csv(city[0] + ".csv")
+    weather = pd.DataFrame(weather, columns=['Day', 'Max', 'Min']) # Labels the columns
+    weather.to_csv(city[0] + ".csv") # Magic one line code for making a csv file
     make_graph(weather, city)
 
 
@@ -65,6 +72,7 @@ get_weather(Eugene_url)
 get_weather(Sydney_url)
 get_weather(London_url)
 
+# Function to show you any days where the temperature is below 0
 def below_zero(url):
     page = requests.get(url)
     data = html.fromstring(page.content)
@@ -79,7 +87,7 @@ def below_zero(url):
     print (city[0] + " temperatures below 0 this week:")
 
     for x, y in zip(day, min):
-        y = int(y)
+        y = int(y) # Similar to the int(i) in get_weather function. Stops wrong datatypes causing errors
         if y <= 0:
             print (x, y)
 
